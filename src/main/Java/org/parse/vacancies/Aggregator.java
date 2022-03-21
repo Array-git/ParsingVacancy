5d03211edb148fdfa65540a7d153bbfc65abbf98
Array-git <array3@gmail.com>
2022-03-21 17:16:11 +0200

    New date format from HH, new search string, lvl from enum

--- src/main/Java/org/parse/vacancies/Aggregator.java
+++ src/main/Java/org/parse/vacancies/Aggregator.java
@@ -13,40 +13,6 @@ public class Aggregator {
         return model.getVacancies(keyWords, city, lvl, remove);
     }
 
-    /*public static final Certificate[] readCertificatesFromPKCS7(byte[] binaryPKCS7Store) throws Exception
-    {
-        try (ByteArrayInputStream bais = new ByteArrayInputStream(binaryPKCS7Store);)
-        {
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            Collection<?> c = cf.generateCertificates(bais);
-                        List<Certificate> certList = new ArrayList<Certificate>();
-
-            if (c.isEmpty())
-            {
-                // If there are now certificates found, the p7b file is probably not in binary format.
-                // It may be in base64 format.
-                // The generateCertificates method only understands raw data.
-            }
-            else
-            {
-
-                Iterator<?> i = c.iterator();
-
-                while (i.hasNext())
-                {
-                    Certificate cert = (Certificate) i.next();
-                    X509CertImpl certificate = new X509CertImpl(cert.getEncoded());
-                    certificate.getSerialNumber();
-                    certList.add((Certificate) i.next());
-                }
-            }
-
-            java.security.cert.Certificate[] certArr = new java.security.cert.Certificate[certList.size()];
-
-            return certList.toArray(certArr);
-        }
-    }*/
-
     public Provider[] toProvider(String[] strategies) {
         List<Provider> providers = new ArrayList<>();
         if (strategies.length == 0) {
