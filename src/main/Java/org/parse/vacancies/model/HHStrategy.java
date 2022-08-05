935f7102f6459436763be0df797dd8ad24c6471b
Array-git <array3@gmail.com>
2022-08-05 15:58:02 +0200

    New className from HH

--- src/main/Java/org/parse/vacancies/model/HHStrategy.java
+++ src/main/Java/org/parse/vacancies/model/HHStrategy.java
@@ -1,5 +1,6 @@
 package org.parse.vacancies.model;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,10 +12,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HHStrategy implements Strategy {
-    private static final String userAgent = "Chrome/97.0.4692.99";
+    private static final String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
     private static final String referrer = "https://kaliningrad.hh.ru/";
-    //    private static final int timeout = 5 * 1000;
-    private static final String URL_FORMAT = "https://hh.ru/search/vacancy?%s&search_field=name&text=%s+%s+%s&page=%d";
+    private static final int timeout = 5 * 1000;
+    private static final String URL_FORMAT = "https://hh.ru/search/vacancy?%s&search_field=name&search_field=description&text=%s+%s+%s&page=%d";
 
 
     public HHStrategy() {
@@ -27,14 +28,14 @@ public class HHStrategy implements Strategy {
         ConvertDate convertDate = new ConvertDate();
         do {
             Document cacheDoc = getDocument(searchString, cityName, page, lvl, remove);
-            Elements elements = cacheDoc.getElementsByClass("vacancy-serp-item"); //select("div[data-qa=vacancy-serp__vacancy vacancy-serp__vacancy_standard_plus]"); //"data-qa#vacancy-serp__vacancy-title");
+            Elements elements = cacheDoc.getElementsByClass("serp-item"); //select("div[data-qa=vacancy-serp__vacancy vacancy-serp__vacancy_standard_plus]"); //"data-qa#vacancy-serp__vacancy-title");
             if (elements.isEmpty()) break;
             for (Element element : elements) {
                 Vacancy vacancy = new Vacancy();
                 vacancy.setTitle(element.select("a[data-qa=vacancy-serp__vacancy-title]").text());
                 vacancy.setCity(element.select("div[data-qa=vacancy-serp__vacancy-address]").text());
                 vacancy.setCompanyName(element.select("a[data-qa=vacancy-serp__vacancy-employer]").text());
-                vacancy.setSalary(element.getElementsByClass("vacancy-serp-item__sidebar").text());
+                vacancy.setSalary(element.select("[data-qa=vacancy-serp__vacancy-compensation]").text());
                 vacancy.setUrl(element.select("[data-qa=vacancy-serp__vacancy-title]").attr("abs:href"));
                 //vacancy.setDate(element.getElementsByAttributeValue("class","vacancy-serp-item__publication-date vacancy-serp-item__publication-date_long").text());
                 //String data = vacancy.getDate().replaceAll(String.valueOf((char) 160), " ");
@@ -54,7 +55,17 @@ public class HHStrategy implements Strategy {
         try {
             String strRemote = remote ? "schedule=remote" : "";
             String stringForSearch = String.format(URL_FORMAT, strRemote, searchString, LvlHH.valueOf(lvl.name()).getStringLVL(), city, page);
-            return Jsoup.connect(stringForSearch).userAgent(userAgent).referrer(referrer).get();
+            Connection conn = Jsoup.connect(stringForSearch).timeout(timeout);
+            conn.header("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            conn.header("Accept-Encoding", "gzip, deflate, br");
+            conn.header("Accept-Language", "en-US,en;q=0.9");
+            conn.header("Host", "kaliningrad.hh.ru");
+            conn.header("sec-ch-ua-platform", "Windows");
+//            conn.header("Sec-Fetch-Dest", "document");
+//            conn.header("Upgrade-Insecure-Requests", "1");
+            conn.header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36");
+            //return Jsoup.connect(stringForSearch).timeout(timeout).userAgent(userAgent).referrer(referrer).get();
+            return conn.get();
         } catch (IOException e) {
             e.printStackTrace();
         }
